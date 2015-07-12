3a8c3d5a1db697f22c48adeefdaa310748d03b8a
Adriana Callejas <adrica1808@gmail.com>
2015-07-12 01:12:14 -0500

    Se crea funcionalidad CREAR PROPIETARIO y LISTAR PROPIETARIOS, aún está pendiente la parte de la foto.

--- ParqueaderoTecno/src/main/java/co/edu/udistrital/dao/interfaces/PropietarioDAO.java
+++ ParqueaderoTecno/src/main/java/co/edu/udistrital/dao/interfaces/PropietarioDAO.java
@@ -1,8 +1,13 @@
 package co.edu.udistrital.dao.interfaces;
 
+import java.util.List;
+
+import javax.persistence.PersistenceException;
+
 import co.edu.udistrital.dao.GenericDAOInterface;
 import co.edu.udistrital.entidades.Propietario;
+import co.edu.udistrital.entidades.User;
 
 public interface PropietarioDAO extends GenericDAOInterface<Propietario, Long> {
-
+	public List<Propietario> findAllPropietarios() throws PersistenceException;
 }
