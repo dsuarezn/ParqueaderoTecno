fbfcc2bff09272114b41951252db32aa2a8e3d4f
Adriana Callejas <adrica1808@gmail.com>
2015-07-13 18:37:35 -0500

    Falto subir el DTO, el servicio y el controlador

---
+++ ParqueaderoTecno/src/main/java/co/edu/udistrital/business/services/PropietarioService.java
@@ -0,0 +1,20 @@
+package co.edu.udistrital.business.services;
+
+import java.util.List;
+
+import javax.persistence.PersistenceException;
+
+import co.edu.udistrital.entidades.Propietario;
+
+
+public interface PropietarioService {
+	
+	public List<Propietario> findAllPropietarios() throws PersistenceException;
+	
+	public Propietario crearPropietario(Propietario propietario) throws PersistenceException;
+	
+	/*public User actualizarUsuario(User usuario) throws PersistenceException;
+	
+	public void borrarUsuario(User usuario) throws PersistenceException;*/
+
+}
